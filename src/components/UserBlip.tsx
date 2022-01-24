7117d73a99ada7b78375876cba4650899acd3e9b
ulrokx <92767187+ulrokx@users.noreply.github.com>
2022-01-24 16:40:20 -0500

    fake name and pic

--- src/components/UserBlip.tsx
+++ src/components/UserBlip.tsx
@@ -3,8 +3,8 @@ export const UserBlip = ({user}) => {
     const date = dayjs().format("dddd[,] MMMM D");
     return(
         <div className="flex justify-between items-center space-x-4 pl-5">
-            <img src={user.avatar_url} alt="profile picture" className="h-12 w-12 rounded-full outline-2 outline-blue-300 outline-offset-4 outline"/>
-            <h2 className = "text-lg ">Hi {user.name}, today is {date}</h2>
+            <img src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg" alt="profile picture" className="h-12 w-12 rounded-full outline-2 outline-blue-300 outline-offset-4 outline"/>
+            <h2 className = "text-lg ">Hi John Smith, today is {date}</h2>
         </div>
     )
 }
