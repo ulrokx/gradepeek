8c1e551ddbf0785480969ba313fae10e885546da
ulrokx <92767187+ulrokx@users.noreply.github.com>
2022-02-18 17:34:24 -0500

    fix

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -10,7 +10,7 @@ export const TextField = ({value, onChange, disabled = false, placeholder}: ITex
     return (
         <input
             type="text"
-            className="transition-all w-2/3 outline outline-red-300 rounded-md p-3 shadow-md focus:scale-105 focus:outline-4 focus:outline-red-500"
+            className="transition-all w-2/3 outline outline-red-300 rounded-md p-3 shadow-md focus:scale-105 focus:outline-4 focus:outline-red-900"
             onChange={onChange}
             value = {value}
             disabled={disabled}
